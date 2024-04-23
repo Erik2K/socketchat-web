4d2d8016f213921747bf4041552874ecb629632d
Erik2K <erikgallardo2000@gmail.com>
2024-04-23 17:04:44 +0200

    added env socket server

--- app/hooks/useSocket.ts
+++ app/hooks/useSocket.ts
@@ -2,10 +2,10 @@ import { io } from 'socket.io-client'
 import Cookies from 'js-cookie'
 
 const useSocket = () => {
-  // const socksetServer = process.env.SOCKET_URL
+  const socksetServer = process.env.SOCKET_URL
   const session = Cookies.get('session')
 
-  const socket = io('http://localhost:4200', {
+  const socket = io(socksetServer, {
     auth: {
       token: session
     }
