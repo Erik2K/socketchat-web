3f0271f00953f917663cb4dee838239584714902
Erik <65443042+Erik2K@users.noreply.github.com>
2024-05-01 01:32:18 +0200

    fixed errors

--- app/components/Chat/ChatAction/index.tsx
+++ app/components/Chat/ChatAction/index.tsx
@@ -1,46 +1,50 @@
-import React, { useState } from 'react';
-import { Input } from '@nextui-org/input';
-import { Button } from '@nextui-org/button';
-import { Spacer } from '@nextui-org/spacer';
-import styles from '@/app/ui/styles/chat.module.css';
+import React, { useState } from 'react'
+import { Input } from '@nextui-org/input'
+import { Button } from '@nextui-org/button'
+import { Spacer } from '@nextui-org/spacer'
+import styles from '@/app/ui/styles/chat.module.css'
 
 const ChatAction = ({ sendMessage, user }: any) => {
-	const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('')
 
-	const onMessageChange = (message: string) => {
-		setMessage(message);
-	};
+  const onMessageChange = (message: string) => {
+    setMessage(message)
+  }
 
-	const onclickHandler = () => {
-		sendMessage({
-			username: user?.username,
-			message,
-		});
+  const onclickHandler = () => {
+    sendMessage({
+      username: user?.username,
+      message
+    })
 
-		setMessage('');
-	};
+    setMessage('')
+  }
 
-	const handleKeyDown = (e: any) => {
-		if (e.key === 'Enter') {
-			onclickHandler();
-		}
-	};
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      onclickHandler()
+    }
+  }
 
-	return (
-		<div className={styles.chatAction}>
-			<Input
-				type='text'
-				fullWidth
-				value={message}
-				onValueChange={onMessageChange}
-				onKeyDown={handleKeyDown}
-			/>
-			<Spacer x={5} />
-			<Button variant='ghost' size='lg' onClick={onclickHandler}>
-				Send
-			</Button>
-		</div>
-	);
-};
+  return (
+    <div className={styles.chatAction}>
+      <Input
+          type="text"
+          fullWidth
+          value={message}
+          onValueChange={onMessageChange}
+          onKeyDown={handleKeyDown}
+        />
+        <Spacer x={5} />
+        <Button
+          variant="ghost"
+          size="lg"
+          onClick={onclickHandler}
+        >
+          Send
+        </Button>
+    </div>
+  )
+}
 
-export default ChatAction;
+export default ChatAction
